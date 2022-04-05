6a82c1c00ae2def77fa78c09a68d6fa3313ed94e
cryptopal85 <gurkanhuray@gm-macmini.local>
2022-04-05 22:42:17 +0000

    search & download functionality added

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 # Version history: April 05 2022 - Initialising main structure
 #                                - building authentication - preparing routing and html templates
 #
-#
 # Remarks: app.py is a main block that will be used routing the requests such as
 # viewing, sharing, uploading files, etc. to appropriate URLs.
 # End-user facing requests, including input forms will mostly done within relevant html templates.
@@ -24,8 +23,7 @@ import io
 app = Blueprint('app.py', __name__)
 
 
-
-########## Authentication ##########
+##### Authentication #####
 
 
 # Landing page for unauthenticated users, login block shown here
@@ -43,18 +41,81 @@ def index():
 # Flask-login get_id returns the authenticated individual's numeric ID
 # which can be used to extract attributes from classes like DataUser, User, etc. (needs to be clarified)
 
+
 @app.route('/home')
 @login_required
 def presenthome():
-	print("inside presenthome")
+	print("inside present home")
 	if current_user.is_authenticated:
-		uid=current_user.get_id()
-		thisdatauser=DataUser.query.filter_by(userid=uid).first()
+		uid = current_user.get_id()
+		thisdatauser = DataUser.query.filter_by(userid=uid).first()
 	if thisdatauser:
-		dname=thisdatauser.userdisplayname
-		azglist=thisdatauser.authgroups
-		if azglist is None:
-			azglist="No group membership found"
-	return render_template('home-logged.html', displayname=dname, grouplist=azglist)
+		dname = thisdatauser.userdisplayname
+		asglist = thisdatauser.authgroups
+		if asglist is None:
+			asglist = "No group membership found"
+	return render_template('home-logged.html', displayname=dname, grouplist=asglist)
 
 
+##### File Operations #####
+
+# The app's search and download functionality handled here
+# It will be the main block for authorised users when interacting with files
+# Several file types will be presented to users
+# Users will able to carry certain operations like downloading, sharing, deleting, etc.
+
+
+@app.route('/search-download-1', methods=['GET'])
+@login_required
+def presentview():
+	print("inside present view")
+	if current_user.is_authenticated:
+		uid = current_user.get_id()
+		thisdatauser = DataUser.query.filter_by(userid=uid).first()
+	if thisdatauser:
+		asglist = thisdatauser.authgroups
+		duaid = thisdatauser.useraccessid
+		if asglist is None:
+			flash("The user {} is not member of any authorised groups. Please contact the ISS ground centre to get support".format(duaid))
+			asglist = "** No group membership **"
+	return render_template('search.html', grouplist=asglist, aid=duaid)
+
+	
+@app.route('/search-download-1', methods=['POST'])
+@login_required
+def presentview2():
+	print("inside present view2")
+	if current_user.is_authenticated:
+		uid = current_user.get_id()
+		thisdatauser = DataUser.query.filter_by(userid=uid).first()
+	if thisdatauser:
+		thisaid = thisdatauser.useraccessid
+		asglist = thisdatauser.authgroups
+		if asglist is None:
+			flash("The user {} is not member of any authorised groups. Please contact the ISS ground centre to get support").format(thisaid)
+			duserfilegroups = ['122', '124']  # a workaround for users with no membership
+		else:
+			duserfilegroups = getauthsfg(asglist)
+		# Use the authorisation-based function on the SQL userid and groups
+		# Update SQL based on search queries
+		sftype = request.form.get('selectedfiletype')
+		sfname = request.form.get('filename')
+		skeytag = request.form.get('keyword-tag')
+		authsfilesql = getauthsfilesql(uid, duserfilegroups, sftype, sfname, skeytag)
+		if len(sfname) == 0:
+			sfname = "any file name"
+		if len(skeytag) == 0:
+			skeytag = "any keywords"
+		# we need to process the SQL produced above - initialise db connection
+		dbcondata = getconnectiondata()
+		resultslist = getauthsfiles(dbcondata, authsfilesql)
+		if resultslist is not None and len(resultslist) > 0:
+			# let's convert the tuples into a dictionary - what are the tuples? https://www.w3schools.com/python/python_tuples.asp
+			authsdict = newresultsdict(resultslist)
+		else:
+			authsdict = dict()
+			authsdict['00000000000000000000000000000000'] = "No files found for this search: {}".format(sftype)
+	return render_template('view2.html', asfiledict=authsdict, aid=thisaid, grouplist=asglist, searchfname=sfname, searchkeytag=skeytag, searchtype=sftype)
+	
+	
+	
